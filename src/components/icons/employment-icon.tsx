9da67088e25f41e4f1ff63acec3cb600588d259a
Dennis Parulian Panjaitan <drekodraco1@gmail.com>
2025-04-26 22:42:14 +0700

    New

---
+++ src/components/icons/employment-icon.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const EmploymentLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" {...props}>
+    <path d="M7.99967 5.33334V2.66667H5.33301M1.33301 9.33334H2.66634M13.333 9.33334H14.6663M9.99967 8.66667V10M5.99967 8.66667V10M3.99967 5.33334H11.9997C12.7361 5.33334 13.333 5.93029 13.333 6.66667V12C13.333 12.7364 12.7361 13.3333 11.9997 13.3333H3.99967C3.26329 13.3333 2.66634 12.7364 2.66634 12V6.66667C2.66634 5.93029 3.26329 5.33334 3.99967 5.33334Z" 
+    stroke="#3F3F46" strokeWidth="1.33" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+);
+
+export { EmploymentLogo };
